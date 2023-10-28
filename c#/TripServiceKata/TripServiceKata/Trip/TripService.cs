6b4e3244c6fbfb946a7f7f58a8ca98145984b0a7
daniel.guerra <daniel.guerra.gallardo99@gmail.com>
2023-10-28 16:34:00 +0200

    refactor: CheckLoggedUser() func

--- c#/TripServiceKata/TripServiceKata/Trip/TripService.cs
+++ c#/TripServiceKata/TripServiceKata/Trip/TripService.cs
@@ -10,15 +10,19 @@ namespace TripServiceKata.Trip
         {
             List<Trip> tripList = new List<Trip>();
             User.User loggedUser = UserSession.GetInstance().GetLoggedUser();
+            CheckLoggedUser(loggedUser);
 
-            if(loggedUser == null) throw new UserNotLoggedInException(); 
-
-            if(IsFriend(user, loggedUser))
+            if (IsFriend(user, loggedUser))
             {
                 tripList = TripDAO.FindTripsByUser(user);
             }
 
             return tripList;
+        }
+
+        private static void CheckLoggedUser(User.User loggedUser)
+        {
+            if (loggedUser == null) throw new UserNotLoggedInException();
         }
 
         bool IsFriend(User.User user, User.User loggedUser)
